94f94fede5d56650505e17bdc0f7fc2a7437bfdb
gouthamburri19 <gouthamburri@gmail.com>
2021-06-02 13:52:06 -0400

    Spring boot project added.

---
+++ demo-04/src/main/java/com/goutham/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.goutham.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api" )
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello to ALl, By Spring Boot";
+    }
+}
